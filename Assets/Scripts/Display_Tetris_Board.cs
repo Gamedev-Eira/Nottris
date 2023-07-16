460d8340811ec94e7dd309c477ea6882e90be791
Eira <30042565@students.southwales.ac.uk>
2023-07-16 16:44:42 +0100

    Initial commit

---
+++ Assets/Scripts/Display_Tetris_Board.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Display_Tetris_Board : MonoBehaviour {
+
+    char[,] TETRIS_BOARD = new char[24, 10]
+    {
+        {'e','e','e','e','e','e','e','e','e','e'},
+        {'e','e','e','e','e','e','e','e','e','e'},
+        {'e','e','e','e','e','e','e','e','e','e'},
+        {'e','e','e','e','e','e','e','e','e','e'},
+        {'e','e','e','e','e','e','e','e','e','e'},
+        {'e','e','e','e','e','e','e','e','e','e'},
+        {'e','e','e','e','e','e','e','e','e','e'},
+        {'e','e','e','e','e','e','e','e','e','e'},
+        {'e','e','e','e','e','e','e','e','e','e'},
+        {'e','e','e','e','e','e','e','e','e','e'},
+        {'e','e','e','e','e','e','e','e','e','e'},
+        {'e','e','e','e','e','e','e','e','e','e'},
+        {'e','e','e','e','e','e','e','e','e','e'},
+        {'e','e','e','e','e','e','e','e','e','e'},
+        {'e','e','e','e','e','e','e','e','e','e'},
+        {'e','e','e','e','e','e','e','e','e','e'},
+        {'e','e','e','e','e','e','e','e','e','e'},
+        {'e','e','e','e','e','e','e','e','e','e'},
+        {'e','e','e','e','e','e','e','e','e','e'},
+        {'e','e','e','e','e','e','e','e','e','e'},
+        {'e','e','e','e','e','e','e','e','e','e'},
+        {'e','e','e','e','e','e','e','e','e','e'},
+        {'e','e','e','e','e','e','e','e','e','e'},
+        {'e','e','e','e','e','e','e','e','e','e'}
+    };
+
+    void Start() {
+
+    }
+
+    void Update() {
+        
+    }
+
+    void UpdateBoard() {
+
+    }
+
+    void RedrawBoard() {
+
+    }
+
+}
